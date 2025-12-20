c603fde3dbac853702539a1584c16d818d31e8f9
Remy Pereira <remy.pereira@imaginecurve.com>
2025-12-20 15:07:07 +0000

    Remove get_fight_stats function

--- ufc_scraper/utils.py
+++ ufc_scraper/utils.py
@@ -1,11 +1,9 @@
 from collections import defaultdict
 import re
-from typing import Dict, Iterator, List, Tuple, Union
+from typing import Dict, List, Tuple, Union
 from uuid import uuid5, NAMESPACE_URL
 
 from scrapy.http import Response
-
-from entities import FightStats
 
 
 def clean_string(raw_string: str) -> str:
@@ -75,79 +73,3 @@ def get_fight_stats_from_summary(fight_stat_summary: str) -> Tuple[int, int]:
     attempted = int(fight_stat_summary_split[1])
 
     return landed, attempted
-
-
-def get_fight_stats(response: Response) -> Iterator[FightStats]:
-    fighter_url_query = "a.b-link::attr(href)"
-
-    url: str = response.url
-    fight_id = get_uuid_string(url)
-    all_urls: List[str] = response.css(fighter_url_query).getall()
-    fighter_1_url = all_urls[1]
-    fighter_2_url = all_urls[2]
-    fighter_1_id = get_uuid_string(fighter_1_url)
-    fighter_2_id = get_uuid_string(fighter_2_url)
-
-    headers_query = (
-        "thead.b-fight-details__table-head th.b-fight-details__table-col::text"
-    )
-    rows_query = "tbody.b-fight-details__table-body tr.b-fight-details__table-row"
-    values_query = "p.b-fight-details__table-text::text"
-
-    headers = response.css(headers_query).getall()
-    headers_clean = [clean_string(header) for header in headers[1:]]
-
-    rows = response.css(rows_query)
-    summary_stats = rows[0]
-
-    values = summary_stats.css(values_query).getall()
-    values_clean = [clean_string(value) for value in values[4:]]
-
-    fighter_1_values = values_clean[0::2]
-    fighter_2_values = values_clean[1::2]
-    fighter_1_summary_stats_dict = dict(zip(headers_clean, fighter_1_values))
-    fighter_2_summary_stats_dict = dict(zip(headers_clean, fighter_2_values))
-    fighter_1_summary_stats_dict["fighter_id"] = fighter_1_id
-    fighter_2_summary_stats_dict["fighter_id"] = fighter_2_id
-
-    for summary_stats_dict in (
-        fighter_1_summary_stats_dict,
-        fighter_2_summary_stats_dict,
-    ):
-        fighter_id = summary_stats_dict["fighter_id"]
-        fight_stat_id = get_uuid_string(fight_id + fighter_id)
-        (total_strikes_landed, total_strikes_attempted) = get_fight_stats_from_summary(
-            summary_stats_dict["Total str."]
-        )
-        (significant_strikes_landed, significant_strikes_attempted) = (
-            get_fight_stats_from_summary(summary_stats_dict["Sig. str."])
-        )
-        knockdowns = int(summary_stats_dict["KD"])
-        (takedowns_landed, takedowns_attempted) = get_fight_stats_from_summary(
-            summary_stats_dict["Td"]
-        )
-        control_time_raw = clean_string(summary_stats_dict["Ctrl"])
-        (control_time_minutes_string, control_time_seconds_string) = (
-            control_time_raw.split(":")
-        )
-        control_time_minutes = int(control_time_minutes_string)
-        control_time_seconds = int(control_time_seconds_string)
-        submissions_attempted = int(summary_stats_dict["Sub. att"])
-        reversals = int(summary_stats_dict["Rev."])
-
-        yield FightStats(
-            fight_stat_id=fight_stat_id,
-            fight_id=fight_id,
-            fighter_id=fighter_id,
-            total_strikes_landed=total_strikes_landed,
-            total_strikes_attempted=total_strikes_attempted,
-            significant_strikes_landed=significant_strikes_landed,
-            significant_strikes_attempted=significant_strikes_attempted,
-            knockdowns=knockdowns,
-            takedowns_landed=takedowns_landed,
-            takedowns_attempted=takedowns_attempted,
-            control_time_minutes=control_time_minutes,
-            control_time_seconds=control_time_seconds,
-            submissions_attempted=submissions_attempted,
-            reversals=reversals,
-        )
