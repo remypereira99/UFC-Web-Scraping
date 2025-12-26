78c7c127633838945e6b02581d42039657dd343d
Remy Pereira <remy.pereira@imaginecurve.com>
2025-12-26 16:07:37 +0000

    Get fight stat dicts

--- ufc_scraper/parsers.py
+++ ufc_scraper/parsers.py
@@ -626,6 +626,8 @@ class FightStatByRoundParser(FightStatParser):
         )
 
     def parse_response(self) -> Iterator[Any]:
+        self._get_fight_stat_dicts()
+
         for round in range(1, self._num_rounds + 1):
             fighter_1_by_round_stats = self._get_fight_stats_by_round(
                 fighter_id=self._fighter_1_id, round=round
