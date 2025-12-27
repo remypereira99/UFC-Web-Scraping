13c7134ac686a68fd0f9e8e627aaec02f1179ebf
Remy Pereira <remy.pereira@imaginecurve.com>
2025-12-27 12:08:18 +0000

    Yield all rounds in _get_fight_stats_by_round

--- ufc_scraper/ufc_scraper/spiders/fight_stats_by_round.py
+++ ufc_scraper/ufc_scraper/spiders/fight_stats_by_round.py
@@ -31,9 +31,7 @@ class CrawlFightStatsByRound(scrapy.Spider):
 
     def _get_fight_stats_by_round(self, response: Response) -> Any:
         fight_stat_by_round_parser = FightStatByRoundParser(response)
-        fighter_1_stats_by_round, fighter_2_stats_by_round = tuple(
-            fight_stat_by_round_parser.parse_response()
-        )
+        fight_stats_by_round = fight_stat_by_round_parser.parse_response()
 
-        yield fighter_1_stats_by_round
-        yield fighter_2_stats_by_round
+        for fight_stats in fight_stats_by_round:
+            yield fight_stats
