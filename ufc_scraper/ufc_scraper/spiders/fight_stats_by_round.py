56a9160315c0adb26ab327e76ffb876e37b0d848
Remy Pereira <remy.pereira@imaginecurve.com>
2026-01-01 13:10:40 +0000

    Use response.follow

--- ufc_scraper/ufc_scraper/spiders/fight_stats_by_round.py
+++ ufc_scraper/ufc_scraper/spiders/fight_stats_by_round.py
@@ -24,12 +24,12 @@ class CrawlFightStatsByRound(scrapy.Spider):
     def _get_event_urls(self, response: Response) -> Any:
         event_links: List[str] = response.css("a.b-link::attr(href)").getall()
         for link in event_links:
-            yield scrapy.Request(link, callback=self._get_fight_urls)
+            yield response.follow(link, callback=self._get_fight_urls)
 
     def _get_fight_urls(self, response: Response) -> Any:
         fight_links: List[str] = response.css("a.b-flag::attr(href)").getall()
         for link in fight_links:
-            yield scrapy.Request(link, callback=self._get_fight_stats_by_round)
+            yield response.follow(link, callback=self._get_fight_stats_by_round)
 
     def _get_fight_stats_by_round(self, response: Response) -> Any:
         fight_stat_by_round_parser = FightStatByRoundParser(response)
